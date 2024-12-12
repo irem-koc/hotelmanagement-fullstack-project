5ae14d82965ebb11cdd1d5f053163ff1e5e62c5c
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-12 20:18:03 +0300

    Add RoomDto class

---
+++ hotelmanagement-be/src/main/java/com/iremkoc/hotel/hotelmanagement/dto/RoomDto.java
@@ -0,0 +1,20 @@
+package com.iremkoc.hotel.hotelmanagement.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RoomDto {
+
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    private List<BookingDto> bookings;
+}

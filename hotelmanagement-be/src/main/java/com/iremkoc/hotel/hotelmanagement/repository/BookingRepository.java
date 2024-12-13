638bf4b0803ec90ea0c6f08e4613799664a67739
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-13 19:07:45 +0300

    Add methods to BookingRepository interface

--- hotelmanagement-be/src/main/java/com/iremkoc/hotel/hotelmanagement/repository/BookingRepository.java
+++ hotelmanagement-be/src/main/java/com/iremkoc/hotel/hotelmanagement/repository/BookingRepository.java
@@ -1,5 +1,16 @@
 package com.iremkoc.hotel.hotelmanagement.repository;
 
-public interface BookingRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.iremkoc.hotel.hotelmanagement.entity.Booking;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByRoomId(Long roomId);
+
+    List<Booking> findByUserId(Long userId);
+
+    List<Booking> findByConfirmationCode(String confirmationCode);
 
 }

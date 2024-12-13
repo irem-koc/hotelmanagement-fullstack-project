72b195474a5701b54d9e3b77cfbb3ea328e90f25
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-13 19:07:51 +0300

    Add additional methods to RoomRepository

--- hotelmanagement-be/src/main/java/com/iremkoc/hotel/hotelmanagement/repository/RoomRepository.java
+++ hotelmanagement-be/src/main/java/com/iremkoc/hotel/hotelmanagement/repository/RoomRepository.java
@@ -1,5 +1,20 @@
 package com.iremkoc.hotel.hotelmanagement.repository;
 
-public interface RoomRepository {
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.iremkoc.hotel.hotelmanagement.entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
+    List<String> findDistinctRoomTypes();
+
+    @Query("SELECT r FROM Room r WHERE r.id NOT IN(SELECT b.room.id FROM Booking b)")
+    List<Room> getAllAvailableRooms();
+
+    @Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType% AND r.id NOT IN(SELECT b.room.id FROM Booking b WHERE b.checkInDate <= :checkOutDate AND b.checkOutDate >= :checkInDate)")
+    List<Room> findAvaliableRoomsByDatesandTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 }

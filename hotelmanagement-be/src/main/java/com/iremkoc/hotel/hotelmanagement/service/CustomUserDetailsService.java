77d054ded063dde1b90f521297183279056bbdc0
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-13 21:06:37 +0300

    Add CustomUserDetailsService implementation

---
+++ hotelmanagement-be/src/main/java/com/iremkoc/hotel/hotelmanagement/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.iremkoc.hotel.hotelmanagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.iremkoc.hotel.hotelmanagement.exception.OurException;
+import com.iremkoc.hotel.hotelmanagement.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    @Override
+    public UserDetails loadUserByUsername(String username) throws OurException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new OurException("Username/Email not found"));
+    }
+
+}

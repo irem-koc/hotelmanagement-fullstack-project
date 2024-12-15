115cd93e656586e951c88fd55c892d333bec6931
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-15 17:21:21 +0300

    Add AuthController with register and login endpoints

---
+++ hotelmanagement-be/src/main/java/com/iremkoc/hotel/hotelmanagement/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.iremkoc.hotel.hotelmanagement.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iremkoc.hotel.hotelmanagement.dto.LoginRequest;
+import com.iremkoc.hotel.hotelmanagement.dto.Response;
+import com.iremkoc.hotel.hotelmanagement.entity.User;
+import com.iremkoc.hotel.hotelmanagement.service.interfaces.IUserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}

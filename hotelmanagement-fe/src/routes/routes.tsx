d4a249a084e39dc8b7d057b125f2552e638c39ee
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-20 02:11:47 +0300

    Refactor routes and layouts in routes.tsx

--- hotelmanagement-fe/src/routes/routes.tsx
+++ hotelmanagement-fe/src/routes/routes.tsx
@@ -1,26 +1,61 @@
-import { RouteObject } from "react-router";
-import Layout from "../layouts/Layout";
+import { Navigate, RouteObject } from "react-router";
+import AuthLayout from "../layouts/AuthLayout";
+import MainLayout from "../layouts/MainLayout";
+import ProtectedRoute from "./protectedRoute";
+
+// Components
+import FindBooking from "../pages/auth/FindBooking/FindBooking";
+import AuthHome from "../pages/auth/Home/Home";
+import Login from "../pages/auth/Login/Login";
+import Register from "../pages/auth/Register/Register";
+import Rooms from "../pages/auth/Rooms/Rooms";
+
+import Booking from "../pages/main/Booking/Booking";
+import MainHome from "../pages/main/Home/Home";
+import Profile from "../pages/main/Profile/Profile";
+import MainRooms from "../pages/main/Rooms/Rooms";
+
+//TODO: change it
+const isAuthorized = true;
+
+const rootRedirect: RouteObject = {
+  path: "/",
+  element: isAuthorized ? (
+    <Navigate to="/main/home" replace />
+  ) : (
+    <Navigate to="/auth/home" replace />
+  ),
+};
 
 const routes: RouteObject[] = [
+  rootRedirect,
   {
-    path: "/",
-    element: <Layout />,
+    path: "auth",
+    element: <AuthLayout />,
+    children: [
+      { path: "home", element: <AuthHome /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "find-booking", element: <FindBooking /> },
+      { path: "rooms", element: <Rooms /> },
+    ],
+  },
+  {
+    path: "main",
+    element: <ProtectedRoute isAuthorized={isAuthorized} />,
     children: [
       {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "products",
+        path: "",
+        element: <MainLayout />,
         children: [
-          { path: "add", element: <ProductAdd /> },
-          {
-            path: ":id",
-            element: <ProductDetail />,
-          },
-          { path: ":id/edit", element: <ProductEdit /> },
+          { path: "home", element: <MainHome /> },
+          { path: "booking", element: <Booking /> },
+          { path: "profile", element: <Profile /> },
+          { path: "rooms", element: <MainRooms /> },
         ],
       },
     ],
   },
 ];
+
+export default routes;

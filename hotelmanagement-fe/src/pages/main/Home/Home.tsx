77477df46b625553cd5c88c999a5a7e27cae300d
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-20 03:00:28 +0300

    Refactor Home component to use Container component

--- hotelmanagement-fe/src/pages/main/Home/Home.tsx
+++ hotelmanagement-fe/src/pages/main/Home/Home.tsx
@@ -1,12 +1,9 @@
+import Container from "../../../components/Container/Container";
+
 type Props = {};
 
 const Home = (props: Props) => {
-  return (
-    <div className="min-h-screen flex flex-col">
-      {/* Main content area, this will take up the remaining space */}
-      <div className="flex-1 bg-gray-100">Home</div>
-    </div>
-  );
+  return <Container>Home</Container>;
 };
 
 export default Home;

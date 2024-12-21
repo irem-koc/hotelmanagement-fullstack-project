6a6bd6e31e2de0435b75c14d05d49a5e282d0305
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-21 17:21:07 +0300

    Add Redux Provider and Toastify to main.tsx

--- hotelmanagement-fe/src/main.tsx
+++ hotelmanagement-fe/src/main.tsx
@@ -1,13 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App.tsx";
+import Loading from "./components/Loading/Loading.tsx";
 import "./index.css";
+import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <Loading />
+        <App />
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={true}
+          closeOnClick
+          pauseOnHover
+          draggable
+        />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );

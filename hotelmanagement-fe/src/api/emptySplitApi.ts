a64c4b931f64a2c49a73dff12b5b0a5b1444b3d4
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-21 17:21:33 +0300

    Add emptySplitApi module

---
+++ hotelmanagement-fe/src/api/emptySplitApi.ts
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const emptySplitApi = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API }),
+  endpoints: () => ({}),
+});
+export const apiWithTag = emptySplitApi.enhanceEndpoints({
+  addTagTypes: ["users"],
+});

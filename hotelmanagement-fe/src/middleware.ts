9ca47d492a2951822c27696185445a521de01e2e
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-21 17:21:25 +0300

    Add rtkQueryNotificationLogger middleware

---
+++ hotelmanagement-fe/src/middleware.ts
@@ -0,0 +1,36 @@
+import {
+  Middleware,
+  MiddlewareAPI,
+  isFulfilled,
+  isRejectedWithValue,
+} from "@reduxjs/toolkit";
+import { useToast } from "./hooks/useToast";
+
+export const rtkQueryNotificationLogger: Middleware =
+  (api: MiddlewareAPI) => (next) => (action) => {
+    const { notifySuccess, notifyError } = useToast();
+
+    if (isFulfilled(action)) {
+      if (action.payload.statusCode === 200) {
+        notifySuccess({
+          message: `${action.payload.message}`,
+          options: {
+            data: {
+              title: "Success",
+            },
+          },
+        });
+      }
+    } else if (isRejectedWithValue(action)) {
+      notifyError({
+        message: `${action.payload?.data?.message || "Something went wrong!"}`,
+        options: {
+          data: {
+            title: "Hata",
+          },
+        },
+      });
+    }
+
+    return next(action);
+  };

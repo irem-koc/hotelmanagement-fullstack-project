7fdf283e8c01e83dfaacdd5cfe515ddcd826a720
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-26 23:56:12 +0300

    Add CustomInput component

---
+++ hotelmanagement-fe/src/components/CustomInput/CustomInput.tsx
@@ -0,0 +1,24 @@
+type Props = {};
+
+const CustomInput = ({ value, onClick }: Props) => {
+  return (
+    <div className="relative w-full">
+      <input
+        type="text"
+        value={value}
+        onClick={onClick}
+        readOnly
+        className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        placeholder="Select Date"
+      />
+      <span
+        onClick={onClick}
+        className="absolute top-1/2 right-2 transform -translate-y-1/2 cursor-pointer"
+      >
+        📅
+      </span>
+    </div>
+  );
+};
+
+export default CustomInput;

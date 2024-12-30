0759006044f80535ca59029aeaa9221d760df419
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-30 23:56:17 +0300

    Add reducerPath to emptySplitApi

--- hotelmanagement-fe/src/api/emptySplitApi.ts
+++ hotelmanagement-fe/src/api/emptySplitApi.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const emptySplitApi = createApi({
+  reducerPath: "emptySplitApi",
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API }),
   endpoints: () => ({}),
 });

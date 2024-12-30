ed1d626f674a8df88abe34d04b7d0857b8c67981
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-30 20:32:06 +0300

    Add services constants file

---
+++ hotelmanagement-fe/src/constants/services.ts
@@ -0,0 +1,26 @@
+export const services = [
+  {
+    title: "Klima",
+    description:
+      "Odalarımızda bulunan kişisel olarak kontrol edilebilen klima ile konaklamanız boyunca serin ve rahat kalın.",
+    icon: "❄️",
+  },
+  {
+    title: "Mini Bar",
+    description:
+      "Odalarınızda ücretsiz olarak sunulan içecek ve atıştırmalık çeşitleriyle mini barımızın keyfini çıkarın.",
+    icon: "🍾",
+  },
+  {
+    title: "Otopark",
+    description:
+      "Konforunuz için otelimizde otopark hizmeti sunulmaktadır. Vale otopark seçenekleri hakkında bilgi alabilirsiniz.",
+    icon: "🅿️",
+  },
+  {
+    basltitleik: "WiFi",
+    description:
+      "Tüm odalar ve genel alanlarda ücretsiz yüksek hızlı WiFi bağlantısıyla bağlı kalın.",
+    icon: "📶",
+  },
+];

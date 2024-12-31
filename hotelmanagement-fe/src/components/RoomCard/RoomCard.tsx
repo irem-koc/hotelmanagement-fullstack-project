b40110e16a36f33476687880df34bbec6b902f29
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-31 19:47:13 +0300

    Refactor RoomCard component to add navigation functionality

--- hotelmanagement-fe/src/components/RoomCard/RoomCard.tsx
+++ hotelmanagement-fe/src/components/RoomCard/RoomCard.tsx
@@ -1,17 +1,15 @@
-import { Room } from "../../types/RoomType";
+import { useNavigate } from "react-router";
 
-const RoomCard = ({ id, roomType, roomPrice, roomPhotoUrl }: Room) => {
+const RoomCard = ({ id, roomType, roomPrice, roomPhotoUrl, where }) => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col md:flex-row w-full bg-white rounded-lg shadow-lg overflow-hidden border border-gray-300 hover:shadow-xl transition-shadow duration-300">
-      {/* Görsel */}
       <img
         src={roomPhotoUrl}
         alt={`${roomType} photo`}
         className="w-full md:w-1/3 h-56 md:h-auto object-cover"
       />
-      {/* İçerik */}
       <div className="flex flex-col justify-between p-6 w-full">
-        {/* Başlık ve Açıklama */}
         <div>
           <h3 className="text-xl font-bold text-gray-800">{roomType}</h3>
           <p className="text-sm text-gray-500 mt-1">Room ID: {id}</p>
@@ -19,9 +17,11 @@ const RoomCard = ({ id, roomType, roomPrice, roomPhotoUrl }: Room) => {
             ${roomPrice}/night
           </p>
         </div>
-        {/* Buton */}
-        <button className="mt-6 w-full bg-blue-500 text-white py-3 px-6 rounded-md font-medium hover:bg-blue-600 transition-colors duration-300">
-          Book Now
+        <button
+          onClick={() => navigate(where)}
+          className="mt-6 w-full bg-blue-500 text-white py-3 px-6 rounded-md font-medium hover:bg-blue-600 transition-colors duration-300"
+        >
+          View/Book Now
         </button>
       </div>
     </div>

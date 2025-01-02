e922bf7708c20185087695b5e3fbb76c706b38d5
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-02 19:30:15 +0300

    Refactor Rooms component to use hooks and add room filtering

--- hotelmanagement-fe/src/pages/main/Rooms/Rooms.tsx
+++ hotelmanagement-fe/src/pages/main/Rooms/Rooms.tsx
@@ -1,7 +1,44 @@
-type Props = {};
+import { useEffect, useState } from "react";
+import Container from "../../../components/Container/Container";
+import RoomCard from "../../../components/RoomCard/RoomCard";
+import RoomFilter from "../../../components/RoomFilter/RoomFilter";
+import { useGetRoomsQuery } from "../../../hooks/rooms";
+import { Room } from "../../../types/RoomType";
 
-const Rooms = (props: Props) => {
-  return <div>Rooms</div>;
+const Rooms = () => {
+  const { data: rooms, isSuccess } = useGetRoomsQuery();
+  const [roomsData, setRoomsData] = useState<Room[] | null>(null);
+
+  useEffect(() => {
+    if (rooms && rooms.roomList) {
+      setRoomsData(rooms.roomList);
+    }
+  }, [rooms]);
+
+  const handleRoomsData = (data: Room[]) => {
+    setRoomsData(data);
+  };
+
+  return (
+    <Container>
+      <RoomFilter handleData={handleRoomsData} />
+      <div className="w-3/4 mx-auto flex flex-col gap-8 my-8">
+        {isSuccess && roomsData && roomsData.length > 0 ? (
+          roomsData.map((room: Room) => (
+            <RoomCard
+              where={`/main/room-details-book/${room.id}`}
+              key={room.id}
+              {...room}
+            />
+          ))
+        ) : (
+          <p className="text-center text-gray-600 col-span-full">
+            No rooms available.
+          </p>
+        )}
+      </div>
+    </Container>
+  );
 };
 
 export default Rooms;

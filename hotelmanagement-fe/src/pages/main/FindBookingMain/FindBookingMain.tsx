c41ef438affc5f0da92facb9f8f9838816ac5115
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-02 19:28:28 +0300

    Add FindBookingMain component

---
+++ hotelmanagement-fe/src/pages/main/FindBookingMain/FindBookingMain.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import BookingDetailsUI from "../../../components/BookingDetailsUI/BookingDetailsUI";
+import { useLazyGetBookingByConfirmationCodeQuery } from "../../../hooks/bookings";
+
+const FindBookingMain = () => {
+  const [bookingCode, setBookingCode] = useState("");
+  const [trigger, { data, isSuccess }] =
+    useLazyGetBookingByConfirmationCodeQuery();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBookingCode(e.target.value);
+  };
+
+  const handleSearch = () => {
+    if (bookingCode.trim()) {
+      trigger(bookingCode);
+      setBookingCode("");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4 p-6 bg-white rounded-lg shadow-lg max-w-2xl mx-auto mt-8">
+      <input
+        value={bookingCode}
+        onChange={handleChange}
+        type="text"
+        placeholder="Search for booking"
+        className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <button
+        onClick={handleSearch}
+        className="w-full bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-500 transition duration-300"
+      >
+        Ara
+      </button>
+      {isSuccess && data && <BookingDetailsUI bookingData={data} />}
+    </div>
+  );
+};
+
+export default FindBookingMain;

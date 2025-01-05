7f8f306509d06293c354452fbba01d3f01a88618
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-06 00:24:04 +0300

    Fix CustomInput value and onClick props

--- hotelmanagement-fe/src/components/CustomInput/CustomInput.tsx
+++ hotelmanagement-fe/src/components/CustomInput/CustomInput.tsx
@@ -1,18 +1,18 @@
 type Props = {};
 
-const CustomInput = () => {
+const CustomInput = ({ value, onClick }: Props) => {
   return (
     <div className="relative w-full">
       <input
         type="text"
-        // value={value}
-        // onClick={onClick}
+        value={value}
+        onClick={onClick}
         readOnly
         className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         placeholder="Select Date"
       />
       <span
-        // onClick={onClick}
+        onClick={onClick}
         className="absolute top-1/2 right-2 transform -translate-y-1/2 cursor-pointer"
       >
         📅

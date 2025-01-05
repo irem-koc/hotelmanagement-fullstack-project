309b0427e2358d88aa5231857ad503a92df31f3e
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-06 01:15:14 +0300

    Refactor ManageBookings component to filter bookings by booking code

--- hotelmanagement-fe/src/pages/admin/ManageBookings/ManageBookings.tsx
+++ hotelmanagement-fe/src/pages/admin/ManageBookings/ManageBookings.tsx
@@ -1,33 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookingCard from "../../../components/BookingCard/BookingCard";
-import {
-  useGetAllBookingsQuery,
-  useLazyGetBookingByConfirmationCodeQuery,
-} from "../../../hooks/bookings";
+import { useGetAllBookingsQuery } from "../../../hooks/bookings";
 import { getFromLocalStorage } from "../../../hooks/localStorage";
 
 const ManageBookings = () => {
   const [bookingCode, setBookingCode] = useState("");
+  const [bookings, setBookings] = useState(null);
+
   const token = getFromLocalStorage("user")?.token;
 
   const { data: allData, isSuccess } = useGetAllBookingsQuery(token);
-  const [trigger, { data, isSuccess: isLazySuccess }] =
-    useLazyGetBookingByConfirmationCodeQuery();
+  useEffect(() => {
+    if (allData && allData.bookingList) {
+      setBookings(allData.bookingList);
+    }
+  }, [allData]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBookingCode(e.target.value);
-  };
-
-  const handleSearch = () => {
-    if (bookingCode.trim()) {
-      trigger(bookingCode);
-      setBookingCode("");
+    const inputValue = e.target.value;
+    setBookingCode(inputValue);
+    if (inputValue.length > 0) {
+      setBookings((prev) => {
+        return prev.filter((item) =>
+          item.bookingConfirmationCode
+            .toLowerCase()
+            .includes(inputValue.toLowerCase())
+        );
+      });
+    } else {
+      setBookings(allData.bookingList);
     }
   };
 
   return (
     <div className="flex flex-col items-center justify-start min-h-screen bg-gray-100 p-6 space-y-6">
       <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-2xl">
+        <label>Find by booking code: </label>
         <input
           value={bookingCode}
           onChange={handleChange}
@@ -35,19 +43,13 @@ const ManageBookings = () => {
           placeholder="Search for booking"
           className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-sm"
         />
-        <button
-          onClick={handleSearch}
-          className="mt-4 w-full bg-gradient-to-r from-blue-500 to-blue-700 text-white px-6 py-3 rounded-md hover:from-blue-600 hover:to-blue-800 transition duration-300 shadow-md"
-        >
-          Ara
-        </button>
       </div>
 
-      {isSuccess && allData && (
-        <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-2xl space-y-4">
-          {allData?.bookingList?.map((data, i) => {
-            return <BookingCard key={i} bookingData={data} />;
-          })}
+      {isSuccess && bookings && bookings?.length > 0 && (
+        <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-2xl space-y-6">
+          {bookings?.map((data, i) => (
+            <BookingCard key={i} bookingData={data} />
+          ))}
         </div>
       )}
     </div>

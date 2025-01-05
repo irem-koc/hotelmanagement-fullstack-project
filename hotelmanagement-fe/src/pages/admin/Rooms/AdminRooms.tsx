49e47f267d10730ee2e11d48b09775289283dd2b
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-06 00:26:55 +0300

    Refactor AdminRooms component to handle room data and add useEffect hook

--- hotelmanagement-fe/src/pages/admin/Rooms/AdminRooms.tsx
+++ hotelmanagement-fe/src/pages/admin/Rooms/AdminRooms.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Container from "../../../components/Container/Container";
 import RoomCard from "../../../components/RoomCard/RoomCard";
 import RoomFilter from "../../../components/RoomFilter/RoomFilter";
@@ -5,27 +6,30 @@ import { useGetRoomsQuery } from "../../../hooks/rooms";
 import { Room } from "../../../types/RoomType";
 
 const AdminRooms = () => {
-  const { data: rooms, isLoading, error } = useGetRoomsQuery();
+  const { data: rooms, isSuccess } = useGetRoomsQuery();
+  const [roomsData, setRoomsData] = useState<Room[] | null>(null);
 
-  if (isLoading) {
-    return <p className="text-center text-gray-600">Loading rooms...</p>;
-  }
+  useEffect(() => {
+    if (rooms && rooms.roomList) {
+      setRoomsData(rooms.roomList);
+    }
+  }, [rooms]);
 
-  if (error) {
-    return (
-      <p className="text-center text-red-600">
-        Something went wrong while fetching rooms.
-      </p>
-    );
-  }
+  const handleRoomsData = (data: Room[]) => {
+    setRoomsData(data);
+  };
 
   return (
     <Container>
-      <RoomFilter />
+      <RoomFilter handleData={handleRoomsData} />
       <div className="w-3/4 mx-auto flex flex-col gap-8 my-8">
-        {rooms?.roomList && rooms.roomList.length > 0 ? (
-          rooms.roomList.map((room: Room) => (
-            <RoomCard key={room.id} {...room} />
+        {isSuccess && roomsData && roomsData.length > 0 ? (
+          roomsData.map((room: Room) => (
+            <RoomCard
+              where={`/admin/edit-room/${room.id}`}
+              key={room.id}
+              {...room}
+            />
           ))
         ) : (
           <p className="text-center text-gray-600 col-span-full">

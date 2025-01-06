231acc63cd2cb618b4253de731c9e58b497a17c1
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-06 22:02:13 +0300

    Refactor addNewRoom method signature in IRoomService interface

--- hotelmanagement-be/src/main/java/com/iremkoc/hotel/hotelmanagement/service/interfaces/IRoomService.java
+++ hotelmanagement-be/src/main/java/com/iremkoc/hotel/hotelmanagement/service/interfaces/IRoomService.java
@@ -1,14 +1,14 @@
 package com.iremkoc.hotel.hotelmanagement.service.interfaces;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.iremkoc.hotel.hotelmanagement.dto.AddRoomRequest;
 import com.iremkoc.hotel.hotelmanagement.dto.EditRoomRequest;
 import com.iremkoc.hotel.hotelmanagement.dto.Response;
 
 public interface IRoomService {
-    Response addNewRoom(String photo, String roomType, BigDecimal roomPrice, String roomDescription);
+    Response addNewRoom(AddRoomRequest addRoomRequest);
 
     Response getAllRooms();
 

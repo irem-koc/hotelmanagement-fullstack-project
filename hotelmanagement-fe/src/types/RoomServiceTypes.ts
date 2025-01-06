727cc36d88af41da33964f679daa89618d0b26b0
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-06 04:22:06 +0300

    Add RoomServiceTypes and Remove RoomType

---
+++ hotelmanagement-fe/src/types/RoomServiceTypes.ts
@@ -0,0 +1,25 @@
+export type Room = {
+  id: number;
+  roomType: string;
+  roomPrice: number;
+  roomPhotoUrl: string;
+};
+export type GetAllRoomsResponse = {
+  statusCode: number;
+  message: string;
+  roomList: Room[];
+};
+export type UpdateRoomRequest = {
+  request: {
+    photo: string;
+    roomType: string;
+    roomDescription: string;
+    roomPrice: number;
+  };
+  id: number;
+  token: string;
+};
+export type DeleteRoomRequest = {
+  id: number;
+  token: string;
+};
